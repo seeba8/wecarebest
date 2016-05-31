e766412c41b7a804a12affefc68e12997248b6ac
lisaorzelski <lisa@orzelski.de>
2016-05-31 23:25:36 +0200

    index.html and main.css added, app.js added, partial input pages for index.html added

---
+++ public/js/app.js
@@ -0,0 +1,15 @@
+angular.module('myApp',['ngRoute'])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/', { templateUrl: '../html/partials/home.html' })
+            .when('/search', { templateUrl: '../html/partials/search.html' })
+            .when('/bookingsrequests', { templateUrl: '../html/partials/bookingsrequests.html' })
+            .when('/offerservice', { templateUrl: '../html/partials/offerservice.html' })
+            .when('/about', { templateUrl: '../html/partials/about.html' })
+            .when('/profile', { templateUrl: '../html/partials/profile.html' })
+            .otherwise({ redirectTo: '/'});
+    });
+/*
+    .controller('ArticlesCtrl', function($scope){
+
+    });*/
