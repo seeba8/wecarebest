bd8838a1f790e8225301a1aef0244090fe46f040
cyran <marcel.seibold@de.ibm.com>
2016-06-13 16:40:21 +0200

    created bookingcrtl

---
+++ public/js/BookingCtrl.js
@@ -0,0 +1,8 @@
+ngular.module('myApp').controller('BookingCtrl', [ '$scope', '$http','$window', "auth", function($scope, $http, $window, auth) {
+    var app = this;
+    var url = 'http://localhost:3000';
+    
+    console.log("yeah");
+    
+    
+}]);
