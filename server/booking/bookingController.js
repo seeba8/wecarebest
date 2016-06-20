ef0e7c6a262649ec19dcaf13894c6a384d11ac6c
Layla Martin <laylamartin94@gmail.com>
2016-06-20 16:34:46 +0200

    bookingController changed, read from JWT

--- server/booking/bookingController.js
+++ server/booking/bookingController.js
@@ -2,9 +2,16 @@
  * Created by MS on 13.06.2016.
  */
 
-// Load Schema of Offer
+// Load Schema of Booking and Offer
 var Booking = require('./bookingSchema');
+var Offer = require('../requests_offers/offerSchema');
 var path = require('path');
+var jwt = require('jwt-simple');
+var ExtractJwt = require('passport-jwt').ExtractJwt;
+var JwtStrategy = require('passport-jwt').Strategy;
+var passport = require('passport');
+
+var config = require('../config/passportConfig'); // get config file
 
 //For debugging purposes
 console.log("bookingController File geladen.");
@@ -55,13 +62,50 @@ module.exports.getCreateRequest = function(req, res){
 };*/
 
 module.exports.getMyBookings = function(req, res) {
+    var secretOrKey = config.secret;
     console.log("in new function");
-    console.log("req: " + req + "res: " + res);
-    Booking.find(function(err, bookings){
+
+    conditions = {}; //empty conditions array
+
+    conditions["createdBy"] = "575ec188dcba0f71fd406ec9"; // direct match
+
+    var token = req.headers.authorization;
+    token = token.substr(4);
+
+    var decoded = jwt.decode(token, secretOrKey);
+    console.log(decoded);
+
+    var type = decoded.type;
+
+    if(type == 1) {
+        //caregiver
+        //got to look into offer table first to find match
+        Offer.find().exec(function(err, offers) {
+            console.log("find suitable offers to match bookings");
+
+            console.log(offers);
+            myOffers = {};
+            var arrayLength = offers.length;
+            for (var i = 0; i < arrayLength; i++) {
+                myoffers[i].createdBy = offers[i].createdBy;
+                myoffers[i].id = offers[i].id;
+            }
+        })
+    } else {
+        //careseeker
+        //directly use _id tag
+        conditions["createdBy"] = decoded._id;
+    }
+
+    var opts = {};
+
+    console.log(conditions);
+
+    Booking.find(conditions).exec(function (err, bookings) {
         console.log("Booking find ausgeführt.");
         //console.log(bookings);
         res.send(bookings);
-    })
+    });
 };
 /*module.exports.getMyBookings = function(req, res){
     //depending on user type, send different files!
