5f565f4490de50b41725fa19eb3b90a22146af7e
cyran <marcel.seibold@de.ibm.com>
2016-06-28 15:47:27 +0200

    update layout mybookingscaregiver html

--- public/js/app.js
+++ public/js/app.js
@@ -157,4 +157,23 @@ myApp.filter('datetime1', function($filter) {
     });
 });
 
+myApp.filter('datenotime', function($filter) {
+    return (function(input) {
+        if(input == null){ return ""; }
+        var _date = $filter('date')(new Date(input),
+            'MM/dd/yyyy');
+        return _date.toUpperCase();
+    });
+});
+
+
+myApp.filter('datetotime', function($filter) {
+    return (function(input) {
+        if(input == null){ return ""; }
+        var _date = $filter('date')(new Date(input),
+            'HH:mm UTC');
+        return _date.toUpperCase();
+    });
+});
+
 
